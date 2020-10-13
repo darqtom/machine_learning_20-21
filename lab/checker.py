686054e451c4c6f18d745fc3092e0ad552bebc81
igorsieradzki <sieradzki.igor@gmail.com>
2020-10-13 23:58:54 +0200

    lab 2

---
+++ lab/checker.py
@@ -0,0 +1,60 @@
+import numpy as np
+
+
+
+def check_1_1(mean_error, mean_squared_error, max_error, train_sets):
+    train_set_1d, train_set_2d, train_set_10d = train_sets
+    assert np.isclose(mean_error(train_set_1d, np.array([8])), 8.897352)
+    assert np.isclose(mean_error(train_set_2d, np.array([2.5, 5.2])), 7.89366)
+    assert np.isclose(mean_error(train_set_10d, np.array(np.arange(10))), 14.16922)
+
+    assert np.isclose(mean_squared_error(train_set_1d, np.array([3])), 23.03568)
+    assert np.isclose(mean_squared_error(train_set_2d, np.array([2.4, 8.9])), 124.9397)
+    assert np.isclose(mean_squared_error(train_set_10d, -np.arange(10)), 519.1699)
+
+    assert np.isclose(max_error(train_set_1d, np.array([3])), 7.89418)
+    assert np.isclose(max_error(train_set_2d, np.array([2.4, 8.9])), 14.8628)
+    assert np.isclose(max_error(train_set_10d, -np.linspace(0, 5, num=10)), 23.1727)
+
+
+def check_1_2(minimize_me, minimize_mse, minimize_max, train_set_1d):
+    assert np.isclose(minimize_mse(train_set_1d), -0.89735)
+    assert np.isclose(minimize_mse(train_set_1d * 2), -1.79470584)
+    assert np.isclose(minimize_me(train_set_1d), -1.62603)
+    assert np.isclose(minimize_me(train_set_1d ** 2), 3.965143)
+    assert np.isclose(minimize_max(train_set_1d), 0.0152038)
+    assert np.isclose(minimize_max(train_set_1d / 2), 0.007601903895526174)
+
+
+def check_1_3(me_grad, mse_grad, max_grad, train_sets):
+    train_set_1d, train_set_2d, train_set_10d = train_sets
+    assert all(np.isclose(
+        me_grad(train_set_1d, np.array([0.99])),
+        [0.46666667]
+    ))
+    assert all(np.isclose(
+        me_grad(train_set_2d, np.array([0.99, 8.44])),
+        [0.21458924, 0.89772834]
+    ))
+    assert all(np.isclose(
+        me_grad(train_set_10d, np.linspace(0, 10, num=10)),
+        [-0.14131273, -0.031631, 0.04742431, 0.0353542, 0.16364242, 0.23353252,
+         0.30958123, 0.35552034, 0.4747464, 0.55116738]
+    ))
+
+    assert all(np.isclose(
+        mse_grad(train_set_1d, np.array([1.24])),
+        [4.27470585]
+    ))
+    assert all(np.isclose(
+        mse_grad(train_set_2d, np.array([-8.44, 10.24])),
+        [-14.25378235,  21.80373175]
+    ))
+    assert all(np.isclose(
+        max_grad(train_set_1d, np.array([5.25])),
+        [1.]
+    ))
+    assert all(np.isclose(
+        max_grad(train_set_2d, np.array([-6.28, -4.45])),
+        [-0.77818704, -0.62803259]
+    ))
---
+++ lab/utils.py
@@ -0,0 +1,118 @@
+import numpy as np
+import utils
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib import ticker, cm
+
+
+def get_fn_values(points, fn, X_vals):
+    return np.array([fn(points, v) for v in X_vals])
+
+
+def plot_1d_set(dataset, ax, loss_fns, show_title=False):
+    linspace = np.linspace(dataset.min(), dataset.max(), num=200)
+    ax.set_xlabel("v")
+    ax.set_ylabel("Loss val")
+    ax.scatter(dataset, [0] * len(dataset))
+    for idx, loss_fn in enumerate(loss_fns):
+        y_vals = get_fn_values(dataset, loss_fn, linspace)
+        if show_title:
+            ax.set_title(loss_fn.__name__)
+        ax.plot(linspace, y_vals, label=loss_fn.__name__)
+
+        
+def plot_2d_set(dataset, ax, loss_fn):
+    dataset_mins = dataset.min(0)
+    dataset_maxs = dataset.max(0)
+    first_linspace = np.linspace(dataset_mins[0], dataset_maxs[0], num=40)
+    second_linspace = np.linspace(dataset_mins[1], dataset_maxs[1], num=40)
+    X, Y = np.meshgrid(first_linspace, second_linspace)
+    Z = np.empty_like(X)
+
+    for row_idx, first_coord in enumerate(first_linspace):
+        for col_idx, second_coord in enumerate(second_linspace):
+            Z[row_idx][col_idx] = loss_fn(dataset, np.array([first_coord, second_coord]))
+    ax.plot_surface(X, Y, Z)
+
+    ax.scatter(dataset[:, 0], dataset[:, 1], np.zeros((dataset.shape[0],)))
+
+    
+def contour_2d_set(dataset, ax, loss_fn, linspaces=None):
+    dataset_mins = dataset.min(0)
+    dataset_maxs = dataset.max(0)
+    if linspaces is None:
+        first_linspace = np.linspace(dataset_mins[0], dataset_maxs[0], num=25)
+        second_linspace = np.linspace(dataset_mins[1], dataset_maxs[1], num=25)
+    else:
+        first_linspace, second_linspace = linspaces
+    X, Y = np.meshgrid(first_linspace, second_linspace, indexing="xy")
+    Z = np.empty_like(X)
+
+    for row_idx, first_coord in enumerate(first_linspace):
+        for col_idx, second_coord in enumerate(second_linspace):
+            Z[col_idx][row_idx] = loss_fn(dataset, np.array([first_coord, second_coord]))
+    
+    ax.contour(X, Y, Z, levels=20)
+    if linspaces is None:
+        ax.scatter(dataset[:, 0], dataset[:, 1])
+    else:
+        ax.contourf(first_linspace, second_linspace, Z, levels=300, cmap=cm.PuBu_r)
+    #    plt.colorbar()
+        
+
+def plot_2d_loss_fn(loss_fn, title, dataset):
+    fig = plt.figure(figsize=(10, 4))
+    fig.suptitle(title)
+    ax = fig.add_subplot(1, 2, 1, projection="3d")
+    plot_2d_set(dataset, ax, loss_fn)
+    ax = fig.add_subplot(1, 2, 2)
+    contour_2d_set(dataset, ax, loss_fn)
+    plt.show(fig)
+    plt.close(fig)
+
+
+def plot_minimums(dataset, loss_fns, loss_fns_mins, title):
+    fig, axes = plt.subplots(1, 3, figsize=(15, 4))
+    fig.suptitle(title)
+
+    min_vals = []
+    for (loss_fn, loss_fn_min, ax) in zip(loss_fns, loss_fns_mins, axes):
+        min_val = loss_fn_min(dataset)
+        min_vals += [min_val]
+        ax.scatter(
+            min_val,
+            loss_fn(dataset, min_val),
+            color="black"
+        )
+        plot_1d_set(dataset, ax, [loss_fn], show_title=True)
+
+    plt.show(fig)
+    plt.close(fig)
+    print(
+        "ME minimum: {:.2f} MSE minimum: {:.2f} Max Error minimum: {:.2f}".format(
+            *min_vals)
+    )
+
+
+def plot_gradient_steps_1d(ax, dataset, gradient_descent_fn, grad_fn, loss_fn, num_steps=100, learning_rate=1e-1):
+    final_v, final_grad, all_v = gradient_descent_fn(
+        grad_fn, dataset, num_steps=num_steps, learning_rate=learning_rate)
+    plot_1d_set(dataset, ax, [loss_fn])
+    y_vals = get_fn_values(dataset, loss_fn, all_v)
+    ax.scatter(all_v, y_vals, c=np.arange(len(all_v)), cmap=plt.cm.viridis)
+    return final_v
+
+
+def plot_gradient_steps_2d(ax, dataset, gradient_descent_fn, grad_fn, loss_fn, num_steps=100, learning_rate=1e-2, linspaces=None):
+    final_v, final_grad, all_v = gradient_descent_fn(
+        grad_fn, dataset, num_steps=num_steps, learning_rate=learning_rate)
+    contour_2d_set(dataset, ax, loss_fn, linspaces)
+    ax.scatter(all_v[:, 0], all_v[:, 1], c=np.arange(len(all_v)), cmap=plt.cm.viridis)
+
+    print("Final grad value for {}: {}".format(loss_fn.__name__, final_grad))
+    return final_v
+
+
+
+
+
